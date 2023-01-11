fd1c250048035be7b4fd8ae503fd53ededb58359
TheFullStackFred <fredrikstromberg90@gmail.com>
2023-01-11 18:02:53 +0100

    Added more features

--- index.js
+++ index.js
@@ -2,18 +2,27 @@ const todoInput = document.querySelector('.todo-input')
 const addTodoButton = document.querySelector('.add-todo-button')
 const todoList = document.querySelector('.todo-list')
 
+addTodoButton.disabled = true
+
+todoInput.addEventListener('input', () => {
+  todoInput.value
+    ? (addTodoButton.disabled = false)
+    : (addTodoButton.disabled = true)
+})
+
 addTodoButton.addEventListener('click', () => {
   const todo = document.createElement('li')
   const deleteButton = document.createElement('button')
   deleteButton.className = 'delete-todo-button'
 
   deleteButton.textContent = 'Delete'
-  todo.textContent = todoInput.value
+  todo.innerHTML = `<h2>${todoInput.value}</h2>`
 
   todo.appendChild(deleteButton)
   todoList.appendChild(todo)
 
   todoInput.value = ''
+  addTodoButton.disabled = true
 
   deleteButton.addEventListener('click', () => {
     todoList.removeChild(todo)
